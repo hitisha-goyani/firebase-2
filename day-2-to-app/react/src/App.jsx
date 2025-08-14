52055184baba94d0ec7315a0bb695e9a5082e9bc
hitisha-goyani <goyanihitisha@gmail.com>
2025-08-14 22:42:12 +0530

    make-student

--- day-2-to-app/react/src/App.jsx
+++ day-2-to-app/react/src/App.jsx
@@ -1,13 +1,12 @@
 
 import './App.css'
-import AddStudents from './component/AddStudents'
 import StudentList from './component/StudentList'
 
 function App() {
 
   return (
     <>
-<AddStudents/>
+
 <StudentList/>
     </>
   )
