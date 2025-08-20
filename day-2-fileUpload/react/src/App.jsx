9e99c3c1794cfa02640b9d291fefcc6d5618c8d2
hitisha-goyani <goyanihitisha@gmail.com>
2025-08-20 17:15:29 +0530

    day-2

---
+++ day-2-fileUpload/react/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import CloudFile from './componenent/CloudFile'
+
+function App() {
+ 
+
+  return (
+    <>
+    <CloudFile/>
+    </>
+  )
+}
+
+export default App
---
+++ day-2-fileUpload/react/src/componenent/CloudFile.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CloudFile = () => {
+  return (
+    <>
+
+  <div className='max-w-7xl p-5'>
+        <label class=" mb-2 text-sm font-medium text-gray-900 dark:text-white" for="multiple_files">Upload multiple files</label>
+        <input class="w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="multiple_files" type="file" multiple />
+
+  </div>
+
+    </>
+  )
+}
+
+export default CloudFile
