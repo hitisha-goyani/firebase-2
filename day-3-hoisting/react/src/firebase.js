acd1b11f359c38ee876232d7c1dd8d0df0059aec
hitisha-goyani <goyanihitisha@gmail.com>
2025-08-22 17:08:23 +0530

    day-3

---
+++ day-3-hoisting/react/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCooyTG10Rq4z4bCnJ7xdiPSXLCqflakNM",
+  authDomain: "hoisting-a98c3.firebaseapp.com",
+  projectId: "hoisting-a98c3",
+  storageBucket: "hoisting-a98c3.firebasestorage.app",
+  messagingSenderId: "780390232280",
+  appId: "1:780390232280:web:98fd0946bcf00f81b4a7ad"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
